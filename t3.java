e9e2c439bde17a93ce23aff2d84b25e843e44d42
terrenay <62691118+terrenay@users.noreply.github.com>
2021-01-30 16:07:24 +0100

    Update t3.java
    Bugfix line 58, now handles 0s correctly

--- t3.java
+++ t3.java
@@ -1,11 +1,12 @@
 import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.Random;
 
 public class t3 {
 
 	public static void main(String[] args) {
-		int[] arr = new int[] { 1, 7, 5, 10, 2, 3, 2 };
-
+		int[] arr = new int[] {6, 4, 16, 0, 5, 18, 7, 2, 17, 12, 16, 3, 18, 5, 4, 17, 9, 17, 7, 0};
+		
 		LinkedList<Integer> list1 = new LinkedList<Integer>();
 		LinkedList<Integer> list2 = new LinkedList<Integer>();
 		LinkedList<Integer> list3 = new LinkedList<Integer>();
@@ -39,55 +40,68 @@ public class t3 {
 		} else {
 			System.out.println("Keine Partition");
 		}
-
 	}
 
 	/**
-	 * Returns a list of elements of arr, such that the elements of this list form a subset which sums to k.
+	 * Returns a list of elements of arr, such that the elements of this list form a
+	 * subset which sums to k.
+	 * 
 	 * @param allowed allowed[i] = 1 if and only if arr[i] may be used in a subset.
-	 * @return Either a list of elements of arr if a subset is found, or an empty list otherwise.
+	 * @return Either a list of elements of arr if a subset is found, or an empty
+	 *         list otherwise.
 	 */
 	private static LinkedList<Integer> elementsSubArrayWithSumK(int[] arr, int[] allowed, int k) {
 		int n = arr.length;
 		LinkedList<Integer> output = new LinkedList<Integer>();
 		int[][] dp = new int[n + 1][k + 1];
 		
-		//This function is basically SUBSETSUM with backtracking (to find the partition).
-		
-		//Base Case: 
+		for (int i = 0; i < n; i++) { //quick and dirty bug fix (simply add all 0s to the first subset)
+			if (arr[i] == 0 && allowed[i] == 1) {
+				output.add(0);
+				allowed[i] = 0;
+			}
+		}
+
+		// This function is basically SUBSETSUM with backtracking (to find the
+		// partition).
+
+		// Base Case:
 		for (int i = 1; i <= n; i++) {
-			//If an element is not allowed, we skip its base case
+			// If an element is not allowed, we skip its base case
 			if (allowed[i - 1] == 1 && arr[i - 1] <= k) { // Offsets by 1 are because dp has size [n+1][k+1]
 				dp[i][arr[i - 1]] = 1;
 			}
 		}
-		
-		//Main Loop
+
+		// Main Loop
 		for (int i = 1; i <= n; i++) {
 			for (int j = 1; j <= k; j++) {
-				//Main dp logic: (1) Ignore the new number and copy the value from the line (in the dp table) above it, 
-				//or (2) keep the value set by the base case, or (3) add the new number to subset.
-				//Since "allowed" and "dp" only contain the values 0 and 1, taking the maximum 
-				//corresponds to a logical OR.
+				// Main dp logic: (1) Ignore the new number and copy the value from the line (in
+				// the dp table) above it,
+				// or (2) keep the value set by the base case, or (3) add the new number to
+				// subset.
+				// Since "allowed" and "dp" only contain the values 0 and 1, taking the maximum
+				// corresponds to a logical OR.
 				dp[i][j] = Math.max(dp[i - 1][j],
 						Math.max(dp[i][j], allowed[i - 1] == 1 ? dp[i - 1][Math.max(0, j - arr[i - 1])] : 0));
 			}
 			if (dp[i][k] == 1) {
-				//Everything from here on is just backtracking.
+				// Everything from here on is just backtracking.
 				int curRow = i;
 				int curCol = k;
 				while (curRow > 1 && curCol > 0) {
 					if (allowed[curRow - 1] == 1 && (dp[curRow - 1][Math.max(0, curCol - arr[curRow - 1])] == 1
 							|| curCol - arr[curRow - 1] == 0)) {
-						output.add(arr[curRow - 1]); 
-						//Since arr[curRow-1] is now definitely in this subset, we change the value in "allowed" to 0.
-						allowed[curRow - 1] = 0; 
+						output.add(arr[curRow - 1]);
+						// Since arr[curRow-1] is now definitely in this subset, we change the value in
+						// "allowed" to 0.
+						allowed[curRow - 1] = 0;
 						curCol -= arr[curRow - 1];
 					}
 					curRow--;
 				}
-				//Ugly special case for the first element, basically ignore this lmao.
-				if (dp[curRow][curCol] == 1) { 
+				// Ugly special case for the first element, basically ignore this lmao.
+				if (dp[curRow][curCol] == 1) {
 					output.add(arr[curRow - 1]);
 					allowed[curRow - 1] = 0;
 				}
